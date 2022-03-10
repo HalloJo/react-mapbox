858b5ea0acb00089c7c96fa272371bd4042d3731
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-03-10 14:37:09 +0100

    Create state and add it to Map

--- mapbox/src/App.js
+++ mapbox/src/App.js
@@ -1,12 +1,27 @@
+import React, { Component } from "react";
 import "./App.css";
 import Map from "./Map";
+import Toggler from "./Toggler";
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 52.2995,
+      longitude: 5.2434,
+      style: "mapbox://styles/mapbox/dark-v9",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Toggler />
+        <Map app={this} />
+      </div>
+    );
+  }
 }
 
 export default App;
